60be5d25b781159808d21a0d5d336b3794541466
Troy Bradley <me@bitcointroy.co>
2018-03-27 11:14:53 -0500

    fixed ram.js

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -13,7 +13,7 @@ class RAM {
 	write(MAR, MDR) {
 		// !!! IMPLEMENT ME
 		// write the value in the MDR to the address MAR
-		this.mem(MAR) = MDR;
+		this.mem[MAR] = MDR;
 	}
 
 	/**
@@ -24,7 +24,7 @@ class RAM {
 	read(MAR) {
 		// !!! IMPLEMENT ME
 		// Read the value in address MAR and return it
-		return this.mem(MAR);
+		return this.mem[MAR];
 	}
 }
 
